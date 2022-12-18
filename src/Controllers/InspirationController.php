ffb8a919fbcb998612a4ef9659b2defa3363c720
bekimfetahu <lundrim$01>
2022-12-18 20:09:13 +0000

    test

--- src/Controllers/InspirationController.php
+++ src/Controllers/InspirationController.php
@@ -6,6 +6,7 @@ use Justcrm\Crm\Model\Inspire;
 class InspirationController
 {
     public function __invoke(Inspire $inspire) {
+
         $quote = $inspire->justDoIt();
 
         return view('inspire::index', compact('quote'));
