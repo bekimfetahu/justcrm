031617ee264c1e218be34148d7eb1f0c13abc98b
bekimfetahu <lundrim$01>
2022-12-18 21:47:38 +0000

    Fix composer

--- src/Controllers/InspirationController.php
+++ src/Controllers/InspirationController.php
@@ -8,7 +8,7 @@ class InspirationController
     public function __invoke(Inspire $inspire) {
 
         $quote = $inspire->justDoIt();
-        
+
 
         return view('inspire::index', compact('quote'));
     }
