368d26659065872ef1c2ddf1431869f74e59fcc9
Bekim Fetahu <bekim.fetahu@gmail.com>
2023-03-24 00:15:59 +0000

    Fix Auth login from package

--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
+
 
 
 class LoginController extends Controller
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,9 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Justcrm\Crm\Controllers\LoginController;
 
-
-Route::get('/justcrm/token/{connectionToken}', [LoginController::class,'createToken'])->name('justcrm.token');
-Route::get('/justcrm/sso-access', function (){
-    return config('justcrm.sso_access');
-})->name('justcrm.sso_access');
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/justcrm/token/{connectionToken}', [LoginController::class, 'createToken'])->name('justcrm.token');
+    Route::get('/justcrm/sso-access', function () {
+        return config('justcrm.sso_access');
+    })->name('justcrm.sso_access');
+});
 
