393d0a755742ed328191897b37d6f59cbe7d02ea
bekimfetahu <lundrim$01>
2023-09-21 16:45:45 +0100

    Add chat ext uuid

--- src/config/justcrm.php
+++ src/config/justcrm.php
@@ -4,7 +4,9 @@ $api = $url . '/api/v1';
 $clientId = env('JUSTCRM_CLIENT_ID');
 $clientSecret = env('JUSTCRM_CLIENT_SECRET');
 $customerId = env('JUSTCRM_CUSTOMER_ID');
+$accountUuid = env('JUSTCRM_ACCOUNT_UUID');
 $ssoAccess = $url . '/' . 'sso/' . $customerId . '/access';
+$chatExtUuid = env('JUSTCRM_EX_JUST_CHAT_UUID');
 $token = $api . '/oauth/token';
 
 return [
@@ -14,5 +16,7 @@ return [
     'client_id' => $clientId,
     'client_secret' => $clientSecret,
     'sso_access' => $ssoAccess,
-    'customer_id' => $customerId
+    'customer_id' => $customerId,
+    'account_uuid' => $accountUuid,
+    'ex_just_chat_uuid'=>$chatExtUuid
 ];
